ad3751e27904d298191a9e4fb61582318d57c8cb
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2023-12-29 19:39:37 +0100

    site responsive a 80%

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -9,9 +9,9 @@ export default function Dropdown({index,datafaq}) {
     } 
   return (
     <div  key={index} className='dropdown-item'>
-      <div  className='header-dropdown-item'>
+      <div onClick={()=>handleToggle(index)} className='header-dropdown-item'>
           <h3 className='h3-dropdown'>{datafaq.title}</h3>
-          <img  onClick={()=>handleToggle(index)} src='./fetch.svg' className='img-arrow' style={{ transform:`rotate(${arrowOpen ? 180 : 0}deg)`, transition:'all 0.5s ease'}}  />
+          <img   src='./fetch.svg' className='img-arrow' style={{ transform:`rotate(${arrowOpen ? 180 : 0}deg)`, transition:'all 0.5s ease'}}  />
       </div>
       <div className={arrowOpen ? 'content-dropdown-item-active':'content-dropdown-item'}>  
         <p className='p-description'>
